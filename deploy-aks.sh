ecb1cc0cdedfa0c0a17dd695182b5de207a3e805
Julien Bourque <julien@brqu.me>
2024-01-08 10:17:45 -0500

    Added base url

--- deploy-aks.sh
+++ deploy-aks.sh
@@ -3,6 +3,7 @@ set -e
 
 TEAM_NAME=$1
 ACR_NAME=$2
+DOMAIN_NAME=$3
 
 REPO_NAME="${ACR_NAME}.azurecr.io"
 
@@ -14,16 +15,19 @@ IMAGE_NAME="${REPO_NAME}/${PROJECT_NAME}:${IMAGE_TAG}"
 az acr login -n "$ACR_NAME"
 docker build --target final -t "$IMAGE_NAME" .
 docker push "$IMAGE_NAME"
-az aks get-credentials --resource-group "CS-${TEAM_NAME}-rg" --name "${CLUSTER_NAME}-cluster"
+az aks get-credentials --overwrite-existing --resource-group "CS-${TEAM_NAME}-rg" --name "${CLUSTER_NAME}cluster"
 
-VARIABLES+=("--set=image.repository=\"${REPO_NAME}\"")
+VARIABLES+=("--set=image.repository=\"${REPO_NAME}/${PROJECT_NAME}\"")
 VARIABLES+=("--set=image.tag=\"${IMAGE_TAG}\"")
+VARIABLES+=("--set=ingress.hosts[0].host=\"ai${TEAM_NAME}.${DOMAIN_NAME}\"")
 VARIABLES="$(IFS=" " ; echo "${VARIABLES[*]}")"
 
+
+echo $VARIABLES
 if ! (helm ls  | grep $PROJECT_NAME) then
    echo "Installing Helm Chart"
-   eval helm install $PROJECT_NAME helm/ $VARIABLES
+   eval helm install -f helm/values.yaml $PROJECT_NAME helm/ $VARIABLES
 else
    echo "Upgrading Helm Chart"
-   eval helm upgrade $PROJECT_NAME helm/ $VARIABLES
+   eval helm upgrade -f helm/values.yaml $PROJECT_NAME helm/ $VARIABLES
 fi
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import logger from "winston";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigModule } from "@nestjs/config";
+import * as process from "process";
 
 export class App {
-
+    globalPrefix: string;
     constructor() {
+        ConfigModule.forRoot();
+        this.globalPrefix = process.env.GLOBAL_PREFIX === undefined ? "/" : (process.env.GLOBAL_PREFIX as string);
     }
 
     async init(): Promise<void> {
-        async function bootstrap() {
+        async function bootstrap(globalPrefix: string) {
             const app = await NestFactory.create(AppModule);
+            app.setGlobalPrefix(globalPrefix);
             await app.listen(3000);
         }
 
-        bootstrap();
+        return bootstrap(this.globalPrefix);
     }
 }
